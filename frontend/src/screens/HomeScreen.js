4f7ceb689a71b13b227723825b494f23a99c5548
dcarroll9398 <dcarroll9398@yahoo.com>
2022-03-31 19:17:25 -0500

    Video-12-Product-Details

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import logger from 'use-reducer-logger';
 import axios from 'axios';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -38,6 +39,9 @@ export default function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
