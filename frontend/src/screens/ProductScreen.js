7d3b2a200744506658434da24ee5386370097420
dcarroll9398 <dcarroll9398@yahoo.com>
2022-04-02 10:43:52 -0500

    Video-15-Complete-Add-Tp-Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -44,7 +44,14 @@ export default function ProductScreen() {
     fetchData();
   }, [slug]);
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const addToCartHandler = () => {
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock.');
+    }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity: 1 },
